4a2c4af84d799d0295ad3c5020c512c42fb0a34e
newbSlayr <hiltoncoong@gmail.com>
2024-01-31 19:19:14 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        String filePath = "./data/Dickens_Carol.pdf";
+
+        try {
+            PDDocument document = Loader.loadPDF(new File(filePath));
+
+            // Instantiate PDFTextStripper class
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+
+            // Retrieving text from PDF document
+            String text = pdfStripper.getText(document);
+            System.out.println(text);
+
+            // Closing the document
+            document.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
